2519e0519303fde8d9a0620857768de08ec8bab6
Gabriel <G.Bischof.inf22@stud.bbbaden.ch>
2024-09-13 16:32:48 +0200

    Projektdateien hinzufügen.

---
+++ AuthenticatorAPI/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticatorAPI.Data
+{
+    public class DataContext : IdentityDbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+
+        }
+
+    }
+}
